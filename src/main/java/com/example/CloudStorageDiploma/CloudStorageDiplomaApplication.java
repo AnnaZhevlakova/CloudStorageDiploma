5878cf6e5cceb7245d80f676285d408b4fe19e48
Anna Zhevlakova <azevlakova12@gmail.com>
2025-11-15 14:47:20 +0400

    start

---
+++ src/main/java/com/example/CloudStorageDiploma/CloudStorageDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudStorageDiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageDiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStorageDiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CloudStorageDiploma/CloudStorageDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudStorageDiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageDiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
