7e08eb4ba6c9d207cc598fb626ed314b727e9396
Anna Zhevlakova <azevlakova12@gmail.com>
2025-12-26 19:29:08 +0400

    9

--- src/main/java/com/example/CloudStorageDiploma/configs/SwaggerConfig.java
+++ src/main/java/com/example/CloudStorageDiploma/configs/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.CloudStorageDiploma.configs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,25 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI imageConversionOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String bearerFormat = "JWT";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("")
-                        .description("")
-                        .version("1.0.0"));
+                        .title("Моё API с JWT")
+                        .version("1.0")
+                        .description("API с авторизацией через JWT"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat(bearerFormat)
+                                        .description("Вставьте JWT токен в формате: Bearer <token>")
+                        )
+                );
     }
 
 }
